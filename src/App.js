ba112e1c1eb6ef39c5caeab70bfa8c2b774723d1
afuap <131736706+afuap@users.noreply.github.com>
2024-05-06 12:16:12 -0400

    Added GitHub repository

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Georgina Pobi and is{" "}
+        <a
+          href="https://github.com/afuap/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          opened-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
