3165a0c32a58cefed7f980e84a80e0fba10c3bfe
afuap <131736706+afuap@users.noreply.github.com>
2024-05-06 15:16:18 -0400

    Added basic styling and imported axios

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +28,18 @@ export default function Weather() {
         <li>Monday 17:00</li>
         <li>Mostly Sunny</li>
       </ul>
-      <di className="row">
+      <div className="row">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Sunny"
-          />
-          30°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Sunny"
+              className="float-left"
+            />
+
+            <span className="temperature">30</span>
+            <span className="units">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
@@ -38,7 +48,7 @@ export default function Weather() {
             <li>Wind: 3 km/h</li>
           </ul>
         </div>
-      </di>
+      </div>
     </div>
   );
 }
