6581c9b115cf1ac9e926a923af1a93f7e2e80637
afuap <131736706+afuap@users.noreply.github.com>
2024-05-07 00:20:13 -0400

    Synched icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,12 +14,10 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row">
         <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="{props.data.iconUrl}"
-              alt="{props.data.description}"
-              className="float-left"
-            />
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} />
+            </div>
 
             <span className="temperature">
               {Math.round(props.data.temperature)}
